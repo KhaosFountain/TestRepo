20dba8f7d642ba48fe260cb6e8f460b6f09ebb6f
g00383507 <g00383507@atu.ie>
2022-12-06 21:29:31 +0000

    testing if everything in my notes work...so far good.

---
+++ src/main/java/ie/atu/testFile.java
@@ -0,0 +1,7 @@
+package ie.atu;
+
+public class testFile {
+    public static void main(String[] args) {
+        System.out.println("Testing");
+    }
+}
---
+++ src/test/java/ie/atu/testFileTest.java
@@ -0,0 +1,20 @@
+package ie.atu;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class testFileTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    //whatever testing you haveto do do it here
+    // everytime you might have to create a object so be careful to place it in the setup() test function
+
+    @AfterEach
+    void tearDown() {
+    }
+}
