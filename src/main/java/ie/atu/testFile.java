15b7f8b8c15cf599f2943c3187b87ce78fb70335
g00383507 <g00383507@atu.ie>
2022-12-06 21:38:41 +0000

    testing yml file

--- src/main/java/ie/atu/testFile.java
+++ src/main/java/ie/atu/testFile.java
@@ -3,5 +3,7 @@ package ie.atu;
 public class testFile {
     public static void main(String[] args) {
         System.out.println("Testing");
+
+        System.out.println("testin yml file");
     }
 }
